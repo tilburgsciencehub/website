0ac8ff1587d58ba267171b835251789db470e512
Ubuntu <ubuntu@ip-172-31-24-206.eu-central-1.compute.internal>
2024-06-25 12:50:22 +0000

    add launch script

---
+++ update_and_restart.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Define variables
+REPO_URL="https://github.com/tilburgsciencehub/website-flask.git"
+CLONE_DIR="/home/ubuntu/tsh-flask"
+
+# Stop the existing Docker containers
+docker compose -f $CLONE_DIR/docker-compose.yml down
+
+# Remove the existing repository (optional: make sure you want to do this)
+rm -rf $CLONE_DIR
+
+# Clone the repository
+git clone $REPO_URL $CLONE_DIR
+
+# Navigate to the clone directory
+cd $CLONE_DIR
+
+# Build the Docker containers
+docker compose build
+
+# Start the Docker containers
+docker compose up -d
+
